6b927447686b25fd60cc821a191542bf11247896
qlxkalf@naver.com <qlxkalf@naver.com>
2024-05-02 10:34:31 +0900

    r

--- ISAC_kaitao/function/get_uav_trajectory_BCD_SCA.m
+++ ISAC_kaitao/function/get_uav_trajectory_BCD_SCA.m
@@ -15,6 +15,24 @@ function uav = get_uav_trajectory_BCD_SCA(distance_user, distance_target, num_us
 
         user_rate = get_user_rate_real(distance_user_l, distance_target_l, num_user, num_target, N, gamma_0, p_max, num_antenna, sensing_th, A_opt, E_opt, uav, PARAM, PARAM.CHANNEL_GAIN);
 
+        uav_t = [
+            270, 200;  % 시작점 q_I
+            330, 350;  % 첫 번째 꼭지점
+            370, 450;  % 두 번째 꼭지점
+            434, 350;  % 두 번째 꼭지점
+            485.2, 270;  % 세 번째 꼭지점
+            530, 200;  % 끝점 q_F
+                ];
+        % 
+        % distance_target_l = get_distance(PARAM.TARGET, uav_t, PARAM.UAV_Z);
+        % distance_user_l = get_distance(PARAM.USER, uav_t, PARAM.UAV_Z);
+        % 
+        % user_rate = get_user_rate_real(distance_user_l, distance_target_l, num_user, num_target, N, gamma_0, p_max, num_antenna, sensing_th, A_opt, E_opt, uav_t, PARAM, PARAM.CHANNEL_GAIN);
+
+
+
+
+
         user_rate_episode_BCD(:,:,episode_BCD) = get_objective(distance_user_l, distance_target_l, num_user, num_target, N, gamma_0, p_max, num_antenna, sensing_th, A_opt, E_opt);
      
         if episode_BCD > 1
