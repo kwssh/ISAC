2d289888b1dfd81fadd75e3315e25e94449c1933
qlxkalf <qlxkalf@naver.com>
2024-07-02 00:21:26 +0900

    d

--- ISAC_kaitao/qwer_tmp.m
+++ ISAC_kaitao/qwer_tmp.m
@@ -6,22 +6,24 @@ function sum_rate_final = qwer()
     %-----------------------------setting parameter-----------------------------------------------------------------------------------------------------------------------------%
     PARAM.SCALING = 1000;
 
-    PARAM.NUM_USER = 4;
-    PARAM.NUM_TARGET = 4;
+    PARAM.NUM_USER = 2;
+    PARAM.NUM_TARGET = 1;
     PARAM.NUM_ANTENNA = 16;
     PARAM.NUM_EPISODE = 10^(6);
 
-    PARAM.USER = [250 400; 350 450; 450 450; 550 400];
-    PARAM.UAV_START = [270 200];
-    PARAM.UAV_END = [530 200];
+    % PARAM.USER = [250 400; 350 450; 450 450; 550 400];
+    PARAM.USER = [0 50; 200 200];
+    PARAM.UAV_START = [-10 0];
+    PARAM.UAV_END = [10 0];
     PARAM.UAV_Z = 40;
-    PARAM.TARGET = [320 160; 360 120; 440 120; 480 160];
+    PARAM.TARGET = [0 100];
 
     PARAM.NOISE_POWER = 10^(-10);
     PARAM.NOISE_POWER_SCALING = PARAM.NOISE_POWER  * PARAM.SCALING^2;
 
     PARAM.SENSING_TH_db = -7;
-    PARAM.SENSING_TH = 10^(0.1 * PARAM.SENSING_TH_db) * 10^(-3) * 0;
+    % PARAM.SENSING_TH = 10^(0.1 * PARAM.SENSING_TH_db) * 10^(-3);
+    PARAM.SENSING_TH = 12 * 10^(-5) * 0;
     PARAM.SENSING_TH_SCALING = PARAM.SENSING_TH * PARAM.SCALING^2;
 
     PARAM.RATE_TH = 0.25;
@@ -31,15 +33,15 @@ function sum_rate_final = qwer()
     PARAM.CHANNEL_GAIN = 10^(-3);
     PARAM.GAMMA = PARAM.CHANNEL_GAIN / PARAM.NOISE_POWER;
 
-    PARAM.TOTAL_TIME = 80;
-    PARAM.TOTAL_DURATION = 4;
-    PARAM.TOTAL_TIME_SLOT = PARAM.TOTAL_TIME / PARAM.TOTAL_DURATION;
+    PARAM.TOTAL_TIME = 3;                                                    % T
+    PARAM.TOTAL_DURATION = 1;                                              % delta_t
+    PARAM.TOTAL_TIME_SLOT = PARAM.TOTAL_TIME / PARAM.TOTAL_DURATION;          % N
 
-    PARAM.ISAC_TIME = 20;
-    PARAM.ISAC_TIME_SLOT_NUM = PARAM.TOTAL_TIME / PARAM.ISAC_TIME;
-    PARAM.ISAC_DURATION = PARAM.TOTAL_TIME_SLOT / PARAM.ISAC_TIME_SLOT_NUM;
+    PARAM.ISAC_TIME = 3;                                                     % T_L
+    PARAM.ISAC_TIME_SLOT_NUM = PARAM.TOTAL_TIME / PARAM.ISAC_TIME;            % L
+    PARAM.ISAC_DURATION = PARAM.TOTAL_TIME_SLOT / PARAM.ISAC_TIME_SLOT_NUM;   % N_L
 
-    PARAM.V_MAX = 30;
+    PARAM.V_MAX = 1000;
     PARAM.ETA = 10^(-4);
     PARAM.RATE_TH = 0.25;
     %----------------------------------------------------------------------------------------------------------------------------------------------------------------------------%
@@ -49,7 +51,12 @@ function sum_rate_final = qwer()
     for episode = 1 : PARAM.NUM_EPISODE
         
         if episode == 1
-           [old_A_opt, old_E_opt, old_A_bar_opt, old_E_bar_opt, old_uav] = get_init(PARAM.UAV_START(1), PARAM.UAV_END(1), PARAM.UAV_START(2), PARAM.TOTAL_TIME_SLOT, PARAM.NUM_USER, PARAM.NUM_TARGET, PARAM.ISAC_DURATION);
+           [old_A_opt, old_E_opt, old_A_bar_opt, old_E_bar_opt, old_uav] = get_init(PARAM.UAV_START(1), PARAM.UAV_END(1), PARAM.UAV_START(2), PARAM.TOTAL_TIME_SLOT, PARAM.NUM_USER, PARAM.NUM_TARGET, PARAM.ISAC_DURATION, PARAM);
+           % [old_A_opt, old_E_opt, old_A_bar_opt, old_E_bar_opt, old_uav] = get_init_fix(PARAM.UAV_START(1), PARAM.UAV_END(1), PARAM.UAV_START(2), PARAM.TOTAL_TIME_SLOT, PARAM.NUM_USER, PARAM.NUM_TARGET, PARAM.ISAC_DURATION, PARAM);
+
+           [old_A_bar_opt, old_E_bar_opt] = get_slack_variable(old_A_opt, old_E_opt);
+
+
         end
 
         distance_user = get_distance(PARAM.USER, old_uav, PARAM.UAV_Z);
@@ -57,25 +64,15 @@ function sum_rate_final = qwer()
     
         [new_A_bar_opt, new_E_bar_opt] = get_slack_variable(old_A_opt, old_E_opt);
         [new_A_opt, new_E_opt] = get_association(old_A_bar_opt, old_E_bar_opt, PARAM.NUM_ANTENNA, PARAM.P_MAX, distance_user, PARAM.NUM_USER, distance_target, PARAM.NUM_TARGET, PARAM.SENSING_TH, PARAM.TOTAL_TIME_SLOT, PARAM.ETA, PARAM.GAMMA, PARAM.ISAC_DURATION, PARAM.RATE_TH);
-        
-        % [new_A_opt, new_E_opt] = get_period_tmp(old_A_bar_opt, old_E_bar_opt, PARAM.NUM_ANTENNA, PARAM.P_MAX, distance_user, PARAM.NUM_USER, distance_target, PARAM.NUM_TARGET, PARAM.SENSING_TH, PARAM.N, PARAM.ETA, PARAM.N_L, PARAM.L, PARAM.RATE_TH, PARAM.GAMMA);
-        % [new_A_opt, new_E_opt] = get_period(old_A_bar_opt, old_E_bar_opt, PARAM.NUM_ANTENNA, PARAM.P_MAX, distance_user, PARAM.NUM_USER, distance_target, PARAM.NUM_TARGET, PARAM.SENSING_TH, PARAM.N, PARAM.ETA, PARAM.N_L, PARAM.L, PARAM.RATE_TH, PARAM.GAMMA);
-    
-        %new_uav = get_uav_trajectory(old_A_opt, old_E_opt, old_A_bar_opt, old_E_bar_opt, distance_user, distance_target, PARAM.NUM_USER, PARAM.NUM_TARGET, PARAM.N, PARAM.GAMMA, PARAM.P_MAX, PARAM.NUM_ANTENNA, PARAM.SENSING_TH);
+
         [new_uav, user_rate] = get_uav_trajectory_BCD_SCA(distance_user, distance_target, PARAM.NUM_USER, PARAM.NUM_TARGET, PARAM.TOTAL_TIME_SLOT, PARAM.GAMMA, PARAM.P_MAX, PARAM.NUM_ANTENNA, PARAM.SENSING_TH, PARAM, old_uav, PARAM.V_MAX, PARAM.TOTAL_DURATION, old_A_opt, old_E_opt, PARAM.RATE_TH, PARAM.ISAC_DURATION);
 
         new_distance_user = get_distance(PARAM.USER, new_uav, PARAM.UAV_Z);
         new_distance_target = get_distance(PARAM.TARGET, new_uav, PARAM.UAV_Z);
 
-        % new_distance_user = get_distance(PARAM.USER, old_uav, PARAM.UAV_Z);
-        % new_distance_target = get_distance(PARAM.TARGET, old_uav, PARAM.UAV_Z);
-    
         objective_val_episode(episode) = get_objective_val(new_distance_user, new_distance_target, PARAM.NUM_USER, PARAM.TOTAL_TIME_SLOT, PARAM.GAMMA, PARAM.P_MAX, PARAM.NUM_ANTENNA, PARAM.SENSING_TH, new_A_opt, new_E_opt, new_A_bar_opt, new_E_bar_opt, PARAM.ETA, PARAM.NUM_TARGET);
-        % objective_val_episode(episode) = get_objective_val(new_distance_user, new_distance_target, PARAM.NUM_USER, PARAM.TOTAL_TIME_SLOT, PARAM.GAMMA, PARAM.P_MAX, PARAM.NUM_ANTENNA, PARAM.SENSING_TH, old_A_opt, old_E_opt, old_A_bar_opt, old_E_bar_opt, PARAM.ETA, PARAM.NUM_TARGET);
-        % objective_val_episode(:,:,episode) = get_user_rate_real(new_distance_user, new_distance_target, PARAM.NUM_USER, PARAM.NUM_TARGET, PARAM.TOTAL_TIME_SLOT, PARAM.GAMMA, PARAM.P_MAX, PARAM.NUM_ANTENNA, PARAM.SENSING_TH, old_A_opt, old_E_opt, new_uav, PARAM, PARAM.CHANNEL_GAIN);
-   
         if episode > 1
-            if abs(sum(sum(objective_val_episode(episode))) - sum(sum(objective_val_episode(episode-1)))) < 0.01
+            if abs(sum(sum(objective_val_episode(episode))) - sum(sum(objective_val_episode(episode-1)))) < 0.00001
                 break
             end
         end
